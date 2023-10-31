312c497a8d47d3d139343c2426ee128591a41147
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-10-31 18:50:18 +0530

    Create Expense.js

---
+++ model/Expense.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+var expenseSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ["Fuel", "Food", "Electricity", "Movie"],
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Expense", expenseSchema);
